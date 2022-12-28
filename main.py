4ae078075b078b8090f3d585a06e14f1772fe27d
Roman <11-roman@mail.ru>
2022-12-28 21:43:27 +0300

    add multiplication and division

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 while True:
     a = int (input('Введите первое число: '))
     b = int (input('Введите второе число: '))
-    c = input('Выберите действие: \n1. + \n2. - \n')
+    c = input('Выберите действие: \n1. + \n2. - \n3. * \n4. / \n')
 
     s = a + b
     m = a - b
+    u = a * b
+    d = a / b
 
     if c == '1':
         print ('Ваш результат: ' + str (s))
     if c == '2':
         print ('Ваш результат: ' + str (m))
+    if c == '3':
+        print ('Ваш результат: ' + str (u))
+    if c == '4':
+        print ('Ваш результат: ' + str (d))
 
-    d = input ('')
+    qw = input ('')
