0fb34f0094cf9c68faa0d5dc4c609b25962c3180
Roman <11-roman@mail.ru>
2022-12-30 21:49:41 +0300

    add color to my answer and menu

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
 '''I want to make bot-helper in console. It will be give me ready answer to client\'s questions'''
 
+from colorama import Fore, Back, Style
+
 while True:
     print ('\n1. Магазин')
     print ('2. Доставка')
@@ -24,17 +26,18 @@ while True:
 
         qu1 = input ('\nВыберите действие: ')
         if qu1 == '1':
-            print ('\nВаш ответ 1')
+            print (Fore.RED + '\nВаш ответ 1' + Style.RESET_ALL)
         if qu1 == '2':
-            print ('\nВаш ответ 2')
+            print (Fore.RED + '\nВаш ответ 2' + Style.RESET_ALL)
         if qu1 == '3':
-            print ('\nВаш ответ 3')
+            print (Fore.RED + '\nВаш ответ 3' + Style.RESET_ALL)
         if qu1 == '4':
-            print ('\nВаш ответ 4')
+            print (Fore.RED + '\nВаш ответ 4' + Style.RESET_ALL)
         if qu1 == '5':
-            print ('\nВаш ответ 5')
+            print (Fore.RED + '\nВаш ответ 5' + Style.RESET_ALL)
 
-    qu0 = input ('\nДля продолжения нажмите Enter, для выхода 0: ')
+    qu0 = input ('\nДля продолжения нажмите ' + Fore.RED + 'Enter' + Style.RESET_ALL + ', для выхода ' + Fore.RED + '0'
+                 + Style.RESET_ALL + ': ')
     if qu0 == '0':
-        print('\nДо встречи!')
+        print(Fore.RED + '\nДо встречи!' + Style.RESET_ALL)
         break
