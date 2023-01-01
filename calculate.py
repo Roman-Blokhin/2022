07c742dbf2fdda488efbacb3d8002b37f7acc113
Roman <11-roman@mail.ru>
2023-01-01 20:22:20 +0300

    make def for button to compact view

--- calculate.py
+++ calculate.py
@@ -1,7 +1,6 @@
 """I want to do calculate with graphic design by tkinter"""
 
 from tkinter import *
-from unicodedata import digit
 
 
 def add_digit(digit):
@@ -12,6 +11,10 @@ def add_digit(digit):
     e.insert(0, value)
 
 
+def make_button (digit):
+    return Button(text=digit, bd=5, font=('Arial', 15, 'normal'), command=lambda: add_digit (digit))
+
+
 root = Tk()
 root.geometry('250x250+200+200')
 root.config(bg='grey')
@@ -20,16 +23,16 @@ root.title('Calculate')
 e = Entry(root, bd=5, justify=RIGHT, font = ('Arial', 15, 'normal'))
 e.grid(row=0, column=0, columnspan=4, stick='wens', padx=5, pady=5)
 
-Button(text='1', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (1)).grid(row=1, column=0, stick='wens', padx=3, pady=1)
-Button(text='2', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (2)).grid(row=1, column=1, stick='wens', padx=3, pady=1)
-Button(text='3', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (3)).grid(row=1, column=2, stick='wens', padx=3, pady=1)
-Button(text='4', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (4)).grid(row=2, column=0, stick='wens', padx=3, pady=1)
-Button(text='5', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (5)).grid(row=2, column=1, stick='wens', padx=3, pady=1)
-Button(text='6', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (6)).grid(row=2, column=2, stick='wens', padx=3, pady=1)
-Button(text='7', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (7)).grid(row=3, column=0, stick='wens', padx=3, pady=1)
-Button(text='8', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (8)).grid(row=3, column=1, stick='wens', padx=3, pady=1)
-Button(text='9', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (9)).grid(row=3, column=2, stick='wens', padx=3, pady=1)
-Button(text='0', bd=5, font = ('Arial', 15, 'normal'), command=lambda: add_digit (0)).grid(row=4, column=0, stick='wens', padx=3, pady=1)
+make_button (1).grid(row=1, column=0, stick='wens', padx=3, pady=1)
+make_button (2).grid(row=1, column=1, stick='wens', padx=3, pady=1)
+make_button (3).grid(row=1, column=2, stick='wens', padx=3, pady=1)
+make_button (4).grid(row=2, column=0, stick='wens', padx=3, pady=1)
+make_button (5).grid(row=2, column=1, stick='wens', padx=3, pady=1)
+make_button (6).grid(row=2, column=2, stick='wens', padx=3, pady=1)
+make_button (7).grid(row=3, column=0, stick='wens', padx=3, pady=1)
+make_button (8).grid(row=3, column=1, stick='wens', padx=3, pady=1)
+make_button (9).grid(row=3, column=2, stick='wens', padx=3, pady=1)
+make_button (0).grid(row=4, column=0, stick='wens', padx=3, pady=1)
 
 root.grid_columnconfigure(0, minsize=50)
 root.grid_columnconfigure(1, minsize=50)
